a9185070bf046dae5529cb55cf6cbf022e846209
Caesar <caesario@gagasimaji.com>
2024-01-31 21:29:59 +0700

    test lagi dong

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
@@ -74,7 +74,7 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
                                 as="button"
                                 className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
-                                Click here to re-send the verification email.
+                                Klik dong buat reset
                             </Link>
                         </p>
 
